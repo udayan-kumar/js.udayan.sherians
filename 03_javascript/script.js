f888d0817baeaedda87d5061f0563b734bba08e0
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-08 01:17:37 +0530

    advance javascript

--- 03_javascript/script.js
+++ 03_javascript/script.js
@@ -78,15 +78,98 @@
 
 // call aply bind --> function ko call karte time hum set kar sakte hai uski this ki value kya hogi
 
-let obj = {
-    name: "udayan",
-    age: 17,
-};
+// let obj = {
+//     name: "udayan",
+//     age: 17,
+// };
 
-function kumar(){
-    console.log(this.name);
-}
+// function kumar(){
+//     console.log(this.name);
+// }
 
-kumar.call(obj);
+// kumar.call(obj);
 
-2:15
+
+// OOPS --> humein factory banana hai , matlab ki aap ek baar blueprint bana do ki har object kaisa dikhega and hum log naye naye object with different values bana paayenge , yhii OOPS ka uper uper se pura kaam hai
+
+// function Udayan(name,price,quantity,color,category){
+//     this.name = name;
+//     this.price = price;
+//     this.quantity = quantity;
+//     this.color = color;
+//     this.category = category;
+//     this.write = function(text){
+//         let h1 = document.createElement("h1");
+//         h1.textContent = text;
+//         h1.style.color = color;
+//         document.body.append(h1);
+//     };
+// }
+
+// let biscuit = new Udayan("oreo",10,10,"red","choclate biscuit");
+// let biscuit2 = new Udayan("marigold",10,10,"blue","normal");
+
+
+// PROTOTYPES --> agar tumhara constructor function koi field apne protype par attach karle to us constructor se banne wale sabhi new instances yaani ki object, ke pass wo field automatically chali jaati hai
+
+// function Udayan(name,price,quantity,color,category){
+//     this.name = name;
+//     this.price = price;
+//     this.quantity = quantity;
+//     this.color = color;
+//     this.category = category;
+    
+// }
+// Udayan.prototype.write = function(text){
+//         let h1 = document.createElement("h1");
+//         h1.textContent = text;
+//         h1.style.color = this.color;
+//         document.body.append(h1);
+//     };
+
+// let biscuit = new Udayan("oreo",10,10,"red","choclate biscuit");
+// let biscuit2 = new Udayan("marigold",10,10,"blue","normal");
+
+// CLASS
+
+// class Udayan{
+//     constructor(name, company, price, color){
+//         this.name = name;
+//         this.company = company;
+//         this.price = price;
+//         this.color = color;
+//     }
+
+//     write(text){
+//         let h1 = document.createElement("h1");
+//         h1.textContent = text;
+//         h1.style.color =this.color;
+//         document.body.appendChild(h1);
+//     }
+// }
+
+// let p1 = new Udayan("natraj","natraj", 10,"blue");
+
+// CLASSICAL INHERITANCE --> classes banana aur unhe extend kar dena,, pichhle class ki value le lena,, ake class dusre class se kuchh leti hai
+
+// PROTOTYPE INHERITANCE --> object to object inheritance ,, ake object dusre object se kuchh le lena ake object hai aap chaho to uski sarri props/methods ko inherit kara dete ho doosre object mein  
+
+// let coffee = {
+//     color: "dark",
+//     drink: function(){
+//         console.log("chup chap pii lo");
+//     }
+// }
+
+// let arabita = Object.create(coffee);
+// // arabita.taste = "not good";
+// arabita.drink();
+
+// koi bhi code javascript mein line by line chalega aur ye natural pattern hai ki code line by line chale ,, but kabhi kabar aise cases aate hai life mein jaha par aapka code wait karta hai and until der mein agla code chal jata hai
+
+// sync --> aisa code jo line by line chale wo sync code hota hai
+
+// async --> aisa code jo jab chale ke liye ready ho jaaye tab chale wo async code
+
+// console.log(Math.floor(Math.random()*10));
+3:34
