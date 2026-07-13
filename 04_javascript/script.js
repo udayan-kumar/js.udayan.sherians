19a27cb3d86cb1c29c2adc78c2f2a4d9bd59ab9a
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-14 01:41:06 +0530

    design pattern

--- 04_javascript/script.js
+++ 04_javascript/script.js
@@ -64,31 +64,72 @@
 
 // is pattern ka main idea hai --> object creation ko ake function ke through control karna ;; har baar jab tum factory function call karte ho , tumhe ake naya object milta hai jisme apne methods aur (agar chaho to) private data ho sakta hai
 
-function createProduct(name , price) {
-    let stock = 20;
+// function createProduct(name , price) {
+//     let stock = 20;
 
-    return {
-        name,
-         price, 
-         checkstock(){
-            console.log(stock);
-        },
+//     return {
+//         name,
+//          price, 
+//          checkstock(){
+//             console.log(stock);
+//         },
 
-        buy(quantity){
-            if(quantity <= stock){
-                stock -= quantity;
-                console.log(`${quantity} pieces booked - ${stock} pieces left`);
-            }
-            else {
-                console.error(`we have only ${stock} pieces left`);
-            }
-        },
+//         buy(quantity){
+//             if(quantity <= stock){
+//                 stock -= quantity;
+//                 console.log(`${quantity} pieces booked - ${stock} pieces left`);
+//             }
+//             else {
+//                 console.error(`we have only ${stock} pieces left`);
+//             }
+//         },
 
-        refil(quantity){
-            stock += quantity;
-            console.log(`refiled the stock - ${stock} pieces now`);
-        },
-    };
+//         refil(quantity){
+//             stock += quantity;
+//             console.log(`refiled the stock - ${stock} pieces now`);
+//         },
+//     };
+// }
+
+// let iphone = createProduct("iphne" , 70000);
+// iphone.buy(12);
+
+// let oppo = createProduct("oppo",30000)
+// oppo.buy(13);
+
+class YouTubeChannel{
+    constructor(){
+        this.subscribes = [];
+    }
+
+    subscribe(user){
+        this.subscribes.push(user);
+            user.update(`${user.name}, you have subscribed the channel..`);
+    }
+
+    unsubscribed(user){
+        this.subscribes = this.subscribers.filter((sub) => sub !== user);
+        user.update(`you have unsubscribed the channel..`);
+    }
+ 
+    notify(message){
+      this.subscribes.forEach((sub) => sub.update(message));
+    }
 }
 
-let iphone = createProduct("iphne" , 70000);
+class user{
+    constructor(name){
+        this.name = name;
+    }
+
+    update(data){
+        console.log(`${this.name}, ${data}`);
+    }
+}
+
+let udayan = new YouTubeChannel();
+let user1 = new user("udayan singh");
+
+udayan.subscribe(user1);
+udayan.notify("new video uploaded");
+ 
