769ac4649341a7596bb6a702f4d7dbda7ca78bd9
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-27 01:05:13 +0530

    debouncing

--- 04_javascript/script.js
+++ 04_javascript/script.js
@@ -1,6 +1,6 @@
 // DESIGN PATTERN
 
-// MODULE PATTERN --> module pattern ake design pattern hai jisme hum apne code ko self executing function (IIFE) ke ander likhte hai , taki variable aur function private rahe,, eske ander se whi cheeze return karte hsi jise bahar use karte hain,, is pattern ka main fayeda hai data hiding (encapsulation) aur clean structure , reasuable aur mangebale ban sake....
+//! MODULE PATTERN --> module pattern ake design pattern hai jisme hum apne code ko self executing function (IIFE) ke ander likhte hai , taki variable aur function private rahe,, eske ander se whi cheeze return karte hsi jise bahar use karte hain,, is pattern ka main fayeda hai data hiding (encapsulation) aur clean structure , reasuable aur mangebale ban sake....
 
 // let Bank = (function(){
 //     let bankbalnce = 12000;
@@ -30,7 +30,7 @@
 
 
 
-// REVEALING MODULE FUNCTION --> i can fix name of what come inside
+//! REVEALING MODULE FUNCTION --> i can fix name of what come inside
 
 // let Bank = (function(){
 //     let bankbalnce = 12000;
@@ -60,9 +60,9 @@
 
 
 
-// factory function --> ake function bante hai jo object create karta hai (factory = object banane ki machine);;  factory function pattern ake aisa design pattern hai jisme hum ake simple function likhte hai jo naye objects banakar return karta hai , bina class ya keyword use kiye hue;;; 
+//! factory function --> ake function bante hai jo object create karta hai (factory = object banane ki machine);;  factory function pattern ake aisa design pattern hai jisme hum ake simple function likhte hai jo naye objects banakar return karta hai , bina class ya keyword use kiye hue;;; 
 
-// is pattern ka main idea hai --> object creation ko ake function ke through control karna ;; har baar jab tum factory function call karte ho , tumhe ake naya object milta hai jisme apne methods aur (agar chaho to) private data ho sakta hai
+//! is pattern ka main idea hai --> object creation ko ake function ke through control karna ;; har baar jab tum factory function call karte ho , tumhe ake naya object milta hai jisme apne methods aur (agar chaho to) private data ho sakta hai
 
 // function createProduct(name , price) {
 //     let stock = 20;
@@ -97,39 +97,60 @@
 // let oppo = createProduct("oppo",30000)
 // oppo.buy(13);
 
-class YouTubeChannel{
-    constructor(){
-        this.subscribes = [];
-    }
+// class YouTubeChannel{
+//     constructor(){
+//         this.subscribes = [];
+//     }
 
-    subscribe(user){
-        this.subscribes.push(user);
-            user.update(`${user.name}, you have subscribed the channel..`);
-    }
+//     subscribe(user){
+//         this.subscribes.push(user);
+//             user.update(`${user.name}, you have subscribed the channel..`);
+//     }
 
-    unsubscribed(user){
-        this.subscribes = this.subscribers.filter((sub) => sub !== user);
-        user.update(`you have unsubscribed the channel..`);
-    }
+//     unsubscribed(user){
+//         this.subscribes = this.subscribers.filter((sub) => sub !== user);
+//         user.update(`you have unsubscribed the channel..`);
+//     }
  
-    notify(message){
-      this.subscribes.forEach((sub) => sub.update(message));
-    }
+//     notify(message){
+//       this.subscribes.forEach((sub) => sub.update(message));
+//     }
+// }
+ 
+// class user{
+//     constructor(name){
+//         this.name = name;
+//     }
+
+//     update(data){
+//         console.log(`${this.name}, ${data}`);
+//     }
+// }
+
+// let udayan = new YouTubeChannel();
+// let user1 = new user("udayan singh");
+
+// udayan.subscribe(user1);
+// udayan.notify("new video uploaded");
+
+
+
+//! DEBOUNCING --> AAP KOI ACTION KAR RAHE HO AND AAP YE NHII CHAHTE HAR ACTION PE KUCHH HO , JAB BHI MERE ACTION KE BEECH MEIN KOI SPECIFIC GAP AAJAYE TO FIR REACTION PERFORM HO
+
+function debouncing(fnc , delay){
+    let timer;
+    return function(...args){
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            fnc(...args);
+        }, delay)
+    };
 }
 
-class user{
-    constructor(name){
-        this.name = name;
-    }
+document.querySelector("input").addEventListener("input" , debouncing(function () {
+    console.log("udayan singh");
+}, 1000)
+);
 
-    update(data){
-        console.log(`${this.name}, ${data}`);
-    }
-}
 
-let udayan = new YouTubeChannel();
-let user1 = new user("udayan singh");
 
-udayan.subscribe(user1);
-udayan.notify("new video uploaded");
- 
